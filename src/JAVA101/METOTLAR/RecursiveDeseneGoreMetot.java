82b4e75106c45895f08d35be5f08e818389d5940
sercan <ustunsercan@gmail.com>
2022-06-02 18:14:28 +0300

    feat: recursive desene göre metot added.

---
+++ src/JAVA101/METOTLAR/RecursiveDeseneGoreMetot.java
@@ -0,0 +1,38 @@
+package JAVA101.METOTLAR;
+
+import java.util.Scanner;
+import java.util.concurrent.RecursiveTask;
+
+public class RecursiveDeseneGoreMetot {
+    public static int pattern1(int num) {
+
+        if (num <= 0) {
+            return num;
+        } else {
+            System.out.print(num + " ");
+            return pattern1(num - 5);
+        }
+
+    }
+
+    static int pattern2(int num, int temp) {
+        if (num > temp) {
+            return num;
+        } else {
+            System.out.print(num + " ");
+            return pattern2(num + 5, temp);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.print("Lütfen bir sayı giriniz : ");
+        num = sc.nextInt();
+
+        pattern2(pattern1(num), num);
+
+    }
+}
+
