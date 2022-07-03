c7f21d06158782ed67ff2231620c7e93d5703516
sercan <ustunsercan@gmail.com>
2022-07-03 10:05:40 +0300

    feat: Maas Hesaplayici odevi tamamlandi.

---
+++ src/JAVA101/SINIFLAR/MaasHesaplayici/Main.java
@@ -0,0 +1,8 @@
+package JAVA101.SINIFLAR.MaasHesaplayici;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("sercan", 2000, 45, 1995);
+        employee.run();
+    }
+}
